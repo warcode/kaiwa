59134b9829cf7fb06353f5c313dd91c891c51a08
warcode <warcode@deny.io>
2015-04-12 02:04:12 +0200

    Update mucs.js

--- clientapp/models/mucs.js
+++ clientapp/models/mucs.js
@@ -147,7 +147,6 @@ module.exports = BaseCollection.extend({
                                 }
                             ],
                             function(err, results){
-                                console.log(results);
                                 if(results.indexOf('true') > -1) {
                                     app.mucInfos.push(mucInfo);
                                 }
