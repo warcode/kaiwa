b48dc61d6f80e4f16e4111aedb992f7b8737e98c
warcode <warcode@deny.io>
2015-05-08 19:36:21 +0200

    Update embedIt.js

--- clientapp/helpers/embedIt.js
+++ clientapp/helpers/embedIt.js
@@ -6,15 +6,15 @@ module.exports = function ($html, cb) {
     $($html).find("a.source").oembed(null, {
         fallback : false,
         includeHandle: false,
-        maxWidth: 500,
-        maxHeight: 350,
+        maxWidth: 750,
+        maxHeight: 525,
         afterEmbed: function(container, oembedData) {
             this.parent().parent().parent().show();
         },
         onProviderNotFound: function() {
             var link = $($html).find("a.source");
             var resourceURL = link.attr("href");
-            if (resourceURL.match(/\.(jpg|png|gif)\b/)) {
+            if (resourceURL.match((https:\/\/)[\S]*\.(jpg|png|gif|gifv|webm)\b)) {
                 link.parent().append("<div class='oembedall-container'><a href='" + resourceURL + "' target='_blank'><img src='" + resourceURL + "' / style='max-width:500px; max-height:350px; width: auto; height: auto;'></a></div>");
                 this.parent().parent().show();
             }
